ebe098b27e42dddd8c3a0d12561f324683a7266b
unknown <shuntacurosu@gmail.com>
2020-12-09 22:10:20 +0900

    finished up to sec15

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -9,7 +9,7 @@ import { ProductService } from 'src/app/service/product.service';
   styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
-  product!: Product;
+  product: Product = new Product();
 
   constructor(
     private productService: ProductService,
